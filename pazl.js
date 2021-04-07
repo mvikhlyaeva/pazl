4bf25638f47afe97f856f79e061ac1438a4e67d4
Мария Вихляева <80171807+mviklyaeva@users.noreply.github.com>
2021-04-07 08:15:29 +0300

    Update pazl.js

--- pazl.js
+++ pazl.js
@@ -5,71 +5,70 @@ var numberOfPart;
 
 //Функция, задающая случайный порядок элементов
 var numberOfOrder = [];
-function createAnsortNumberOfOrder(numberOfPart){
 
-for(let i=0;i<numberOfPart*numberOfPart;i++){
-	numberOfOrder[i]=i;
-}
-for(let i=numberOfPart*numberOfPart-1;i>0;i--){
-   let x=Math.floor(Math.random() * i )  
-   	let t=numberOfOrder[i];
-   	numberOfOrder[i]=numberOfOrder[x];
-   	numberOfOrder[x]=t;
-}
+function createAnsortNumberOfOrder(numberOfPart) {
+
+	for (let i = 0; i < numberOfPart * numberOfPart; i++) {
+		numberOfOrder[i] = i;
+	}
+	for (let i = numberOfPart * numberOfPart - 1; i > 0; i--) {
+		let x = Math.floor(Math.random() * i)
+		let t = numberOfOrder[i];
+		numberOfOrder[i] = numberOfOrder[x];
+		numberOfOrder[x] = t;
+	}
 }
 
-b.onclick = function(){
+b.onclick = function () {
 	//Удаляем класс со скриптом меняющим картинки
 	$('.a').detach();
 	let radios = document.querySelectorAll('input[type="radio"]');
 	for (let radio of radios) {
 		if (radio.checked) {
-			 numberOfPart = radio.value;
+			numberOfPart = radio.value;
 		}
 	}
-//удаляем выведенный пазл	
-$('.partOfPazl').detach();	
+	//удаляем выведенный пазл	
+	$('.partOfPazl').detach();
 
-//Добавляем нужное количество частей пазла
-for(let i=0;i<numberOfPart*numberOfPart; i++){
-	$('.pazl').append('<div class="partOfPazl"><img class="partOfPazlImg" src="'+ imgsrc + '"></div>');
+	//Добавляем нужное количество частей пазла
+	for (let i = 0; i < numberOfPart * numberOfPart; i++) {
+		$('.pazl').append('<div class="partOfPazl"><img class="partOfPazlImg" src="' + imgsrc + '"></div>');
+	}
+
+
+	img = document.querySelector(".partOfPazlImg");
+	//console.log(img.naturalWidth);
+	//console.log(img.naturalHeight);
+
+	//Задаем размеры картинки внутри частей пазла
+	var partOfPazl = document.querySelectorAll('.partOfPazl');
+	for (let i = 0; i < partOfPazl.length; i++) {
+		partOfPazl[i].style.width = 100 / numberOfPart + '%';
+		partOfPazl[i].style.height = 100 / numberOfPart + '%';
+	}
+
+	//Задаем отступы для правильного отображения пазла
+	var partOfPazlImg = document.querySelectorAll('.partOfPazlImg');
+	for (let i = 0; i < partOfPazlImg.length; i++) {
+		partOfPazlImg[i].style.margin = (Math.floor(i / numberOfPart) * -(img.naturalHeight / img.naturalWidth * 100) + '% 0 0 ' + ((i % numberOfPart) * -100) + '%');
+		partOfPazlImg[i].style.width = numberOfPart * 100 + '%';
+	}
+
+	createAnsortNumberOfOrder(numberOfPart);
+
+	//Перемешиваем части пазла 
+	for (let i = 0; i < numberOfPart * numberOfPart; i++) {
+		pazl = document.querySelectorAll('.partOfPazl');
+		pazl[i].style.order = +numberOfOrder[i];
+	}
+	//Удаляем поздравительную надпись	
+	$('.q').detach();
+
+	//Добавляем скрипт для замены местами
+	var scripts = document.querySelector('.scripts');
+	var js = document.createElement('script');
+	js.src = "changePart.js";
+	js.classList.add("a");
+	scripts.appendChild(js);
 }
-
-
-img = document.querySelector(".partOfPazlImg");
-//console.log(img.naturalWidth);
-//console.log(img.naturalHeight);
-
-//Задаем размеры картинки внутри частей пазла
-var partOfPazl = document.querySelectorAll('.partOfPazl');
-for(let i=0;i<partOfPazl.length;i++){
-partOfPazl[i].style.width = 100/numberOfPart +'%';
-partOfPazl[i].style.height = 100/numberOfPart +'%';
-}
-
-//Задаем отступы для правильного отображения пазла
- var partOfPazlImg  = document.querySelectorAll('.partOfPazlImg');
- for(let i=0;i<partOfPazlImg.length;i++){
-   partOfPazlImg[i].style.margin = (Math.floor(i/numberOfPart)*-(img.naturalHeight/img.naturalWidth*100) +'% 0 0 '+ ((i%numberOfPart)*-100) +'%');
-     partOfPazlImg[i].style.width = numberOfPart*100 +'%';
-}
-
-createAnsortNumberOfOrder(numberOfPart);
-
-//Перемешиваем части пазла 
-for(let i=0;i<numberOfPart*numberOfPart;i++){
-    pazl  = document.querySelectorAll('.partOfPazl');
-	pazl[i].style.order= +numberOfOrder[i];
-}
-//Удаляем поздравительную надпись	
-$('.q').detach();
-
-//Добавляем скрипт для замены местами
-var scripts = document.querySelector('.scripts');
-var js = document.createElement('script');
-js.src = "changePart.js";
-js.classList.add("a");
-scripts.appendChild(js);
- }
-
-
