8cdfd126c24ba46936ee705e52b08a22d76b9952
Мария Вихляева <80171807+mviklyaeva@users.noreply.github.com>
2021-04-07 08:15:48 +0300

    Update pictureCreat.js

--- pictureCreat.js
+++ pictureCreat.js
@@ -1,31 +1,31 @@
-var numberOfPart=1;
+var numberOfPart = 1;
 var img;
 var imgsrc = 'https://cdn.pixabay.com/photo/2021/03/03/14/55/rhino-6065480_1280.jpg';
 
-var chooseImg=document.querySelectorAll(".chooseImg");
-for(let i=0;i<chooseImg.length;i++){
-	chooseImg[i].onclick = function() {
+var chooseImg = document.querySelectorAll(".chooseImg");
+for (let i = 0; i < chooseImg.length; i++) {
+	chooseImg[i].onclick = function () {
 		imgsrc = chooseImg[i].src;
 		console.log('1');
-		$('.partOfPazl').detach();	
-		$('.pazl').append('<div class="partOfPazl"><img class="partOfPazlImg" src="'+ imgsrc + '"></div>');
+		$('.partOfPazl').detach();
+		$('.pazl').append('<div class="partOfPazl"><img class="partOfPazlImg" src="' + imgsrc + '"></div>');
 	}
 }
 
 
-for(let i=0;i<numberOfPart*numberOfPart; i++){
+for (let i = 0; i < numberOfPart * numberOfPart; i++) {
 	$('.pazl').append('<div class="partOfPazl"><img class="partOfPazlImg" src="https://cdn.pixabay.com/photo/2021/03/03/14/55/rhino-6065480_1280.jpg"></div>');
 }
 
 img = document.querySelector(".partOfPazlImg");
 
-$(function(){
-  $('.pazl').height($('.pazl').width()*img.naturalHeight/img.naturalWidth);
+$(function () {
+	$('.pazl').height($('.pazl').width() * img.naturalHeight / img.naturalWidth);
 
 
-  $(window).resize(function(){
-    $('.pazl').height($('.pazl').width()*img.naturalHeight/img.naturalWidth);
-  });
+	$(window).resize(function () {
+		$('.pazl').height($('.pazl').width() * img.naturalHeight / img.naturalWidth);
+	});
 });
 
 
