7cb34a91c06dfe6658bfc43785535573f121f8e5
Мария Вихляева <80171807+mviklyaeva@users.noreply.github.com>
2021-04-07 08:16:13 +0300

    Update changePart.js

--- changePart.js
+++ changePart.js
@@ -1,30 +1,29 @@
-var pazl  = document.querySelectorAll('.partOfPazl');
-var first=true;
+var pazl = document.querySelectorAll('.partOfPazl');
+var first = true;
 var order;
 var k;
 
-for(let i=0;i<numberOfPart*numberOfPart;i++)
- pazl[i].onclick = function() {
- 	if(first){
- 		console.log("1");
- 		order = pazl[i].style.order;
- 	    k = i;
- 	    first = false;
- 	    // pazl[i].style.border = "3px solid #ababab";
-            $(this).css({border: "3px solid #ababab"})
- 	}
- 	else{
- 		console.log("2");
-        pazl[k].style.order = pazl[i].style.order;
-        pazl[i].style.order = order;
-        first = true;
-        pazl[k].style.border = "none";
-        for(let i =0;i<numberOfPart*numberOfPart;i++){
-        	//console.log(i, pazl[i].style.order);
-        	if(pazl[i].style.order!=i) break;
-        	if(i==numberOfPart*numberOfPart-1) $('.praise').append('<p class="q">Поздравляю, ты молодец!</p>');
+for (let i = 0; i < numberOfPart * numberOfPart; i++)
+	pazl[i].onclick = function () {
+		if (first) {
+			console.log("1");
+			order = pazl[i].style.order;
+			k = i;
+			first = false;
+			pazl[i].style.border = "3px solid #ababab";
+			// $(this).css({
+			// 	border: "3px solid #ababab"
+			// })
+		} else {
+			console.log("2");
+			pazl[k].style.order = pazl[i].style.order;
+			pazl[i].style.order = order;
+			first = true;
+			pazl[k].style.border = "none";
+			for (let i = 0; i < numberOfPart * numberOfPart; i++) {
+				if (pazl[i].style.order != i) break;
+				if (i == numberOfPart * numberOfPart - 1) $('.praise').append('<p class="q">Поздравляю, ты молодец!</p>');
 
-        }
-        //pazl[k].style.cssText = "border: none;";
- 	}  	    	   
-}
+			}
+		}
+	}
